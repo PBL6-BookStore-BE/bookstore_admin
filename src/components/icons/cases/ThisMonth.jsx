5fe5cc7125e984ea81775f36b2f34fcdc6d34fba
Kim Ngoc <hoangkimngoc44@gmail.com>
2022-12-17 18:37:14 +0700

    add StatsContainer

---
+++ src/components/icons/cases/ThisMonth.jsx
@@ -0,0 +1,12 @@
+import { Icon } from '@chakra-ui/react'
+import React from 'react'
+
+const ThisMonth = () => {
+  return (
+    <Icon fill="none" viewBox="0 0 16 16" height="26" width="20" >
+      <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" stroke="#ffffff"></path>
+    </Icon>
+  )
+}
+
+export default ThisMonth
