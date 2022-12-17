058264bb0f1755c6b63359c0152e60cd45d6428c
Kim Ngoc <hoangkimngoc44@gmail.com>
2022-12-17 10:27:32 +0700

    delete sign up

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -70,17 +70,6 @@ const Login = () => {
         <meta charSet="utf-8" />
         <title>Login</title>
       </Helmet>
-      <p className="text-login">
-        Dont have an account?{" "}
-        <Link to="/register" className="text-login-link">
-          Sign up
-        </Link>
-      </p>
-      <Button variant="outline" className="btn-google" mb={5}>
-        <GoogleIcon />
-        <span>Sign in with Google</span>
-      </Button>
-
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
           <LoginForm />
