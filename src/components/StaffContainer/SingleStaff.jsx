eb52ccb20ba242c159eefb638e3c65a35da681d7
Quynh Huong <quynhhuongks2901@gmail.com>
2022-12-27 17:03:28 +0700

    fix: change status action

--- src/components/StaffContainer/SingleStaff.jsx
+++ src/components/StaffContainer/SingleStaff.jsx
@@ -1,75 +1,88 @@
-import { Box, Select, Td, Tr } from '@chakra-ui/react';
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
+import { Box, Select, Td, Tr } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-import { updateState } from '../../store/cases/customer/slice';
+import { updateState } from "../../store/cases/customer/slice";
 
-const SingleStaff = ({ id, isActive, username, fullName, email, phoneNumber, address }) => {
-    const dispatch = useDispatch();
-    const [isDelete, setIsDelete] = useState(isActive);
-    const isDeleteOptions = [
-        { value: true, label: 'Activated'},
-        { value: false, label: 'Disabled'},
-    ]
+const SingleStaff = ({
+  id,
+  isActive,
+  username,
+  fullName,
+  email,
+  phoneNumber,
+  address,
+}) => {
+  const dispatch = useDispatch();
+  const [isDelete, setIsDelete] = useState(isActive);
+  const isDeleteOptions = [
+    { value: true, label: "Activated" },
+    { value: false, label: "Disabled" },
+  ];
   return (
-    <Tr >
-        <Td>{username}</Td>
-        <Td>{fullName}</Td>
-        <Td>{email}</Td>
-        <Td>{address}</Td>
-        <Td>{phoneNumber}</Td>
-        <Td>
-            {isDelete ? (
-                <Box
-                    display="inline-block"
-                    color="#fff"
-                    padding="2px 12px 3px"
-                    fontSize="18px"
-                    backgroundColor="#28ad3a"
-                    borderRadius="9999px"
-                >
-                    Activated
-                </Box>
-            ) : (
-                <Box
-                    display="inline-block"
-                    color="#fff"
-                    padding="2px 12px 3px"
-                    fontSize="18px"
-                    backgroundColor="#f31b1bcb"
-                    borderRadius="9999px"
-                >
-                    Disabled
-                </Box>
-            )}
-        </Td>
-        <Td>
-            <Select 
-                bgColor='#FAFAFA' size='lg' fontSize='md'
-                value={isDelete}
-                onChange={(e) => {
-                    setIsDelete(e.target.value);
-                    try {
-                        dispatch(updateState({
-                            id: id, 
-                            state: !isDelete,
-                        }));
-                        console.log(!isDelete);
-                    } catch(err) {
-                        console.log(err);
-                        toast.error("Can't update status");
-                    }
-                }}
-            >
-                {isDeleteOptions.map((item) => {
-                    return (
-                        <option value={item.value} key={item.value}>{item.label}</option>
-                    )
-                })}
-            </Select>
-        </Td>
+    <Tr>
+      <Td>{username}</Td>
+      <Td>{fullName}</Td>
+      <Td>{email}</Td>
+      <Td>{address}</Td>
+      <Td>{phoneNumber}</Td>
+      <Td>
+        {isDelete ? (
+          <Box
+            display="inline-block"
+            color="#fff"
+            padding="2px 12px 3px"
+            fontSize="18px"
+            backgroundColor="#28ad3a"
+            borderRadius="9999px"
+          >
+            Activated
+          </Box>
+        ) : (
+          <Box
+            display="inline-block"
+            color="#fff"
+            padding="2px 12px 3px"
+            fontSize="18px"
+            backgroundColor="#f31b1bcb"
+            borderRadius="9999px"
+          >
+            Disabled
+          </Box>
+        )}
+      </Td>
+      <Td>
+        <Select
+          bgColor="#FAFAFA"
+          size="lg"
+          fontSize="md"
+          value={isDelete}
+          onChange={(e) => {
+            setIsDelete(e.target.value === "true");
+            try {
+              dispatch(
+                updateState({
+                  id: id,
+                  state: !isDelete,
+                })
+              );
+            } catch (err) {
+              console.log(err);
+              toast.error("Can't update status");
+            }
+          }}
+        >
+          {isDeleteOptions.map((item) => {
+            return (
+              <option value={item.value} key={item.value}>
+                {item.label}
+              </option>
+            );
+          })}
+        </Select>
+      </Td>
     </Tr>
-  )
-}
+  );
+};
 
-export default SingleStaff
+export default SingleStaff;
